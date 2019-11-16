25328e3f218d1429af162e4077c0f8149689806b
SSK0206 <coccinelle.sasa@gmail.com>
2019-11-16 16:53:54 +0900

    メモアプリ作成

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  // テキストフィールドを管理するコントローラを作成(入力内容の取得用)
+  final textController = TextEditingController();
 
-  void _incrementCounter() {
+   // 入力データ格納用のリスト
+  List<Map<String, dynamic>> items = [];
+
+  //　追加ボタンが押されたときの処理（リストにIDと入力データを新規追加）
+  void _addItem(String inputText) {
     setState(() {
       _counter++;
+      items.add({ "id": _counter, "content": inputText});
     });
+  }
+
+  @override
+  // widgetの破棄時にコントローラも破棄
+  void dispose() {
+    textController.dispose();
+    super.dispose();
   }
 
   @override
@@ -45,18 +59,41 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              '入力してください。',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: textController,
+              ),
             ),
+            Expanded(
+              child:ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = items[index];
+                  // 新しいカードを作成して返す
+                  return new Card(
+                    child: ListTile(
+                      title: Text(item["id"].toString() + " : " + item["content"]),
+                    ),
+                  );
+                }),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        // ボタンが押された時の動作
+        onPressed: () {
+          //テキストフィールドの内容を取得し、アイテムリストに追加
+          _addItem(textController.text);
+
+          // テキストフィールドの内容をクリア
+          textController.clear();
+        },
         child: Icon(Icons.add),
       ),
     );
